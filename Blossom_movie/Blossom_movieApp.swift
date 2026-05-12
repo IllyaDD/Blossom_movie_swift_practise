c8e12ad565e3a1b9ea4a0d5df5bb79d85ee096d9
IllyaDD <illya.d.donchenko@gmail.com>
2026-05-12 06:41:34 +0300

    Initial Commit

---
+++ Blossom_movie/Blossom_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blossom_movieApp.swift
+//  Blossom_movie
+//
+//  Created by Illya Donchenko on 12.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Blossom_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
